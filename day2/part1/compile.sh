59712de8965ba4f0429ced521940c83b06ab5f1e
adeci <alex.decious@gmail.com>
2024-12-01 17:47:56 -0500

    fixed setup and input inclusion

--- day2/part1/compile.sh
+++ day2/part1/compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-clang -I../../utils -o solution solution.c ../utils/utils.c
+clang -I../../utils -o solution solution.c ../../utils/utils.c
 if [[ $? -eq 0 ]]; then
     echo "Compilation success."
+    ./solution
 else
     echo "Compilation fail."
 fi
--- setup.sh
+++ setup.sh
@@ -57,6 +57,7 @@ EOF
 clang -I../../utils -o solution solution.c ../../utils/utils.c
 if [[ \$? -eq 0 ]]; then
     echo "Compilation success."
+    ./solution
 else
     echo "Compilation fail."
 fi
